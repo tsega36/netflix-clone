97e65de7b775db1dd301839885770d2133375552
tsega36 <tsegateklewold36@gmail.com>
2025-11-25 11:44:11 +0300

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>netflix-clone</h1>
+    </>
+  )
+}
+
+export default App
